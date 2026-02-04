c1eafe46104c0f825893fd1fd2a407e1427c850a
Jameson <jm@ten2.media>
2026-02-04 14:30:55 -0700

    fix: robust client-side URL normalization without regex literals

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script dangerouslySetInnerHTML={{ __html: `(function(){try{var o=location.origin,p=location.pathname,s=location.search||'',h=location.hash||'';var fixed=p.replace(/\/+/g,'/');if(/\.html$/.test(fixed))fixed=fixed.replace(/\.html$/,'/');if(fixed!==p){var url=o+fixed+s+h;location.replace(url);} }catch(e){} })();` }} />
+        <script dangerouslySetInnerHTML={{ __html: `(function(){try{var o=location.origin,p=location.pathname,s=location.search||'',h=location.hash||'';var parts=p.split('/');for(var i=parts.length-1;i>=0;i--){if(parts[i]==='')parts.splice(i,1);}var fixed='/' + parts.join('/');if(fixed==='')fixed='/';if(fixed.slice(-5)==='.html')fixed=fixed.slice(0,-5)+'/';if(fixed!==p){var url=o+fixed+s+h;location.replace(url);} }catch(e){} })();` }} />
       </head>
       <body>
         <header className="site-header">
